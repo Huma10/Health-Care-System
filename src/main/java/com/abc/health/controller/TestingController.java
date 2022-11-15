11f2cdf94035f23e7d719f9634c9af65017b9160
ABC <ABC@DESKTOP-V6NRKBE>
2022-11-15 15:28:43 +0530

    Added changes in Testing Ctl

--- src/main/java/com/abc/health/controller/TestingController.java
+++ src/main/java/com/abc/health/controller/TestingController.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Controller;
 public class TestingController {
 
 	public void test() {
+		//testing 
+		int a = 10;
+		if(a == 10) System.out.println("Value of a is 10");
 		System.out.println("Hello");
 		
 	}
